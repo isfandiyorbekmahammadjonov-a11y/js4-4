50427db3d78df3857b3a3cae2b538ec1eb1798d0
Isfandiyorbek <isfandiyorbekmahammadjonov@gmail.com>
2026-03-31 22:04:41 +0500

    yangilash

--- app.js
+++ app.js
@@ -17,3 +17,30 @@ function number(num) {
   return num.sort((a, b) => b - a);
 }
 console.log(number(numbers));
+// 5) addExclamation: Add '!' to each word.
+function undov(undov) {
+  return undov.map((item) => item + "!");
+}
+console.log(undov(["cat", "dog", "mushuk", "kuchuk", "parot", "qush"]));
+//6) findFirstShort: Find first word with length < 3
+let three = function (a) {
+  return a.find((item) => item.length < 3);
+};
+console.log(three(["cat", "dog", "mushuk", "kuchuk", "parot", "qush", "va"]));
+//7) checkIncludes: Check if 'banana' exists.
+function fruitNew(fruit) {
+  return fruit.includes("Banan");
+}
+console.log(
+  fruitNew(["Olma", "Nok", "Banan", "Apelsin", "Anor", "Uzum", "O'rik"]),
+);
+//8) getIndexes: Find first and last index of 'b'
+let word = "abcbdbb";
+console.log(word.indexOf("b"), word.lastIndexOf("b"));
+//9) addAndRemove: Add 0 at start, remove last.
+function remove(remove) {
+  remove.unshift("maza");
+  remove.pop();
+  return remove;
+}
+console.log(remove(["cat", "dog", "mushuk", "kuchuk", "parot", "qush"]));
